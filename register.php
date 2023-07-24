ff6858a91a5eac8720dc0b041e4effd5a1c536b6
chirag_xd <kashidchirag2@gmail.com>
2023-07-24 12:55:08 +0530

    Address+Gender

--- register.php
+++ register.php
@@ -23,6 +23,8 @@
         if (isset($_POST["submit"])) {
             $fullname = $_POST["FullName"];
             $email = $_POST["Email"];
+            $address = $_POST["address"];
+            $Gender = $_POST["gender"];
             $password = $_POST["password"];
             $c_paasword = $_POST["ConfirmPassword"];
             $passwordhash = password_hash($password, PASSWORD_DEFAULT);
@@ -35,23 +37,35 @@
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 array_push($errors, "Invalid email format");
             }
+            if (empty($address)) {
+                array_push($errors, "Address Is Necessary");
+            }
+            if (empty($Gender)) {
+                array_push($errors, "Please Select A Gender");
+            }
             if (strlen($password) <= 8) {
                 array_push($errors, "Password Must Be 8 Words Long");
             }
             if ($password !== $c_paasword) {
                 array_push($errors, "Password does Not Match");
             }
-
+            require_once "database.php";
+            $sql = "SELECT * FROM register WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $rowCount = mysqli_num_rows($result);
+            if ($rowCount > 0) {
+                array_push($errors, "Email Already Exist!!");
+            }
             if (count($errors) > 0) {
                 foreach ($errors as $error) {
                     echo "<div class='alert alert-danger'>$error</div>";
                 }
             } else {
-                require_once "database.php";
+
 
                 // Use prepared statement to prevent SQL injection
-                $stmt = $conn->prepare("INSERT INTO register (FullName, Email, `password`) VALUES (?, ?, ?)");
-                $stmt->bind_param("sss", $fullname, $email, $passwordhash);
+                $stmt = $conn->prepare("INSERT INTO register (FullName, Email,address,gender, `password`) VALUES (?, ?, ?, ? ,?)");
+                $stmt->bind_param("sssss", $fullname, $email, $address, $Gender, $passwordhash);
 
                 if ($stmt->execute()) {
                     echo "<div class='alert alert-success'>You've Registered Successfully.</div>";
@@ -66,13 +80,26 @@
         ?>
 
 
-        <form action="regi.php" method="post">
+        <form action="register.php" method="post">
             <div class="form-group">
                 <input type="text" class="form-control" name="FullName" placeholder="Full Name:">
             </div>
             <div class="form-group">
                 <input type="text" class="form-control" name="Email" placeholder="Email:">
             </div>
+            <div class="form-group">
+                <textarea class="form-control" name="address" placeholder="Address:"></textarea>
+            </div>
+
+            <div class="form-group">
+                <input type="radio" name="gender" value="Male">
+                <label for="male">Male</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="radio" name="gender" value="Female">
+                <label for="female">Female</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="radio" name="gender" value="Others">
+                <label for="others">Others</label><br>
+            </div>
+
             <div class="form-group">
                 <input type="password" class="form-control" name="password" placeholder="Password:">
             </div>
@@ -81,10 +108,15 @@
             </div>
             <center>
                 <div class="form-group">
-                    <input type="submit" class="btn btn-primary" value="Submit" name="submit">
+                    <input type="submit" class="btn btn-primary" value="Register" name="submit">
                 </div>
             </center>
         </form>
+        <div>
+            <div>
+                <p>Already Registered <a href="login.php">Login Here</a></p>
+            </div>
+        </div>
     </div>
 </body>
 
